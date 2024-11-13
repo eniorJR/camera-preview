a59da9fce1a27bae244d4ff5679617589acb48e6
uriimarquess <marquesnooriok@gmail.com>
2024-11-13 10:35:20 +0100

    Update Plugin.swift

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -22,6 +22,7 @@ public class CameraPreview: CAPPlugin {
     var enableZoom: Bool?
     var highResolutionOutput: Bool = false
     var disableAudio: Bool = false
+    var mirrorVideo: Bool = false
 
     @objc func rotated() {
         let height = self.paddingBottom != nil ? self.height! - self.paddingBottom!: self.height!
@@ -67,6 +68,7 @@ public class CameraPreview: CAPPlugin {
         self.storeToFile = call.getBool("storeToFile") ?? false
         self.enableZoom = call.getBool("enableZoom") ?? false
         self.disableAudio = call.getBool("disableAudio") ?? false
+        self.mirrorVideo = call.getBool("mirrorVideo") ?? false
 
         AVCaptureDevice.requestAccess(for: .video, completionHandler: { (granted: Bool) in
             guard granted else {
@@ -217,6 +219,17 @@ public class CameraPreview: CAPPlugin {
             }
         }
     }
+    
+    @objc func resume(_ call: CAPPluginCall) {
+        self.cameraController.resume() { error in
+            if let error = error {
+                print(error)
+                call.reject(error.localizedDescription)
+                return
+            }
+            call.resolve()
+        }
+    }
 
     @objc func getSupportedFlashModes(_ call: CAPPluginCall) {
         do {
@@ -261,31 +274,47 @@ public class CameraPreview: CAPPlugin {
         DispatchQueue.main.async {
 
             let quality: Int? = call.getInt("quality", 85)
+            self.mirrorVideo = call.getBool("mirrorVideo") ?? self.mirrorVideo
 
-            self.cameraController.captureVideo { (image, error) in
-
-                guard let image = image else {
-                    print(error ?? "Image capture error")
-                    guard let error = error else {
-                        call.reject("Image capture error")
-                        return
-                    }
-                    call.reject(error.localizedDescription)
+            self.cameraController.captureVideo(mirror: self.mirrorVideo) { (error) in
+                guard let error = error else {
+                    call.resolve()
                     return
                 }
-
-                // self.videoUrl = image
-
-                call.resolve(["value": image.absoluteString])
+                call.reject(error.localizedDescription)
+                return
             }
         }
     }
+@objc func stopRecordVideo(_ call: CAPPluginCall) {
+    self.cameraController.stopRecording { (video, error) in
+        guard let video = video else {
+            print(error ?? "Video capture error")
+            guard let error = error else {
+                call.reject("Video capture error")
+                return
+            }
+            call.reject(error.localizedDescription)
+            return
+        }
 
-    @objc func stopRecordVideo(_ call: CAPPluginCall) {
+        print(video.absoluteString + " Video end >")
 
-        self.cameraController.stopRecording { (_) in
-
+        do {
+            // Convierte el archivo de video a Data
+            let videoData = try Data(contentsOf: video)
+            // Convierte los datos a base64
+            let base64String = videoData.base64EncodedString()
+            // Responde con la URL y el archivo en base64
+            call.resolve([
+                "videoFilePath": video.absoluteString,
+                "videoBase64": base64String
+            ])
+        } catch {
+            // Maneja errores al leer el archivo de video
+            call.reject("Error reading video file: \(error.localizedDescription)")
         }
     }
+}
 
 }
